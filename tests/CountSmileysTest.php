7ef4e1d10f6f9dc72bc255c2db31972726fd7f01
Andrey Shklyaruk <13mailg@gmail.com>
2020-04-17 16:50:00 +0300

    Created CountSmileysTest

---
+++ tests/CountSmileysTest.php
@@ -0,0 +1,27 @@
+<?php
+
+/*
+Given an array as an argument complete the function that should return the total number of smiling faces.
+Rules for a smiling face:
+- Each smiley face must contain a valid pair of eyes. Eyes can be marked as : or ;
+- A smiley face can have a nose but it does not have to. Valid characters for a nose are - or ~
+- Every smiling face must have a smiling mouth that should be marked with either ) or D.
+No additional characters are allowed except for those mentioned.
+ */
+
+use App\CountSmileys;
+use PHPUnit\Framework\TestCase;
+
+class CountSmileysTest extends TestCase
+{
+    /** @test */
+    public function it_works()
+    {
+        $counter = new CountSmileys;
+
+        $this->assertEquals(0, $counter->count([]));
+        $this->assertEquals(4, $counter->count([':D',':~)',';~D',':)']));
+        $this->assertEquals(2, $counter->count([':)',':(',':D',':O',':;']));
+        $this->assertEquals(1, $counter->count([';]', ':[', ';*', ':$', ';-D']));
+    }
+}
