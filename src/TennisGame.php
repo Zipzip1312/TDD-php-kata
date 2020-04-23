2fafc10a9034c0cc48bdc10ab301e09ac7708c12
Andrey Shklyaruk <13mailg@gmail.com>
2020-04-23 13:48:12 +0300

    TennisGame kata completed

--- src/TennisGame.php
+++ src/TennisGame.php
@@ -4,8 +4,131 @@ namespace App;
 
 class TennisGame
 {
-    public function score()
+    protected $playerOnePoints = 0;
+    protected $playerTwoPoints = 0;
+
+    /**
+     * Score the match.
+     *
+     * @return string
+     */
+    public function score(): string
     {
-        return 'love-love';
+        if ($this->hasWinner()) {
+            return 'Winner: '.$this->leader();
+        }
+
+        if ($this->hasAdvantage()) {
+            return 'Advantage: '.$this->leader();
+        }
+
+        if ($this->isDeuce()) {
+            return 'deuce';
+        }
+
+        return sprintf(
+            "%s-%s",
+            $this->pointsToTerm($this->playerOnePoints),
+            $this->pointsToTerm($this->playerTwoPoints),
+        );
+    }
+
+    /**
+     * Add point to Player One
+     */
+    public function pointToPlayerOne(): void
+    {
+        $this->playerOnePoints++;
+    }
+
+    /**
+     * Add point to Player Two
+     */
+    public function pointToPlayerTwo(): void
+    {
+        $this->playerTwoPoints++;
+    }
+
+    /**
+     * Get the current leader of the set.
+     *
+     * @return Player
+     */
+    protected function leader(): string
+    {
+        return $this->playerOnePoints > $this->playerTwoPoints
+            ? 'Player One'
+            : 'Player Two';
+    }
+
+    /**
+     * Determine if the players are in deuce.
+     *
+     * @return bool
+     */
+    protected function isDeuce(): bool
+    {
+        if (! $this->hasReachedDeuceThreshold()) {
+            return false;
+        }
+
+        return $this->playerOnePoints === $this->playerTwoPoints;
+    }
+
+    /**
+     * Determine if both players have scored at least 3 points.
+     *
+     * @return bool
+     */
+    protected function hasReachedDeuceThreshold(): bool
+    {
+        return $this->playerOnePoints >= 3 && $this->playerTwoPoints >= 3;
+    }
+
+    /**
+     * Determine if one player has the advantage.
+     *
+     * @return bool
+     */
+    protected function hasAdvantage(): bool
+    {
+        if (! $this->hasReachedDeuceThreshold()) {
+            return false;
+        }
+
+        return ! $this->isDeuce();
+    }
+
+    /**
+     * Determine if there is a winner.
+     *
+     * @return bool
+     */
+    protected function hasWinner(): bool
+    {
+        if ($this->playerOnePoints < 4 && $this->playerTwoPoints < 4) {
+            return false;
+        }
+
+        return abs($this->playerOnePoints - $this->playerTwoPoints) >= 2;
+    }
+
+    /**
+     * Convert the player's score to the Tennis term.
+     *
+     * @return stringa
+     */
+    protected function pointsToTerm($points): string
+    {
+        switch ($points) {
+            case 0:
+                return 'love';
+            case 1:
+                return 'fifteen';
+            case 2:
+                return 'thirty';
+            case 3:
+                return 'forty';
+        }
     }
 }
--- tests/TennisGameTest.php
+++ tests/TennisGameTest.php
@@ -20,10 +20,41 @@ use PHPUnit\Framework\TestCase;
 
 class TennisGameTest extends TestCase
 {
-    /** @test */
-    public function it_scores_0_to_0()
+    /**
+     * @test
+     * @dataProvider scores
+     */
+    function it_scores_a_tennis_match($playerOnePoints, $playerTwoPoints, $score)
     {
-        $game = new Game;
-        $this->assertEquals('love-love', $game->score());
+        $match = new Game();
+
+        for ($i = 0; $i < $playerOnePoints; $i++) {
+            $match->pointToPlayerOne();
+        }
+
+        for ($i = 0; $i < $playerTwoPoints; $i++) {
+            $match->pointToPlayerTwo();
+        }
+
+        $this->assertEquals($score, $match->score());
+    }
+
+    public function scores()
+    {
+        return [
+            [0, 0, 'love-love'],
+            [1, 0, 'fifteen-love'],
+            [1, 1, 'fifteen-fifteen'],
+            [2, 0, 'thirty-love'],
+            [3, 0, 'forty-love'],
+            [2, 2, 'thirty-thirty'],
+            [3, 3, 'deuce'],
+            [4, 4, 'deuce'],
+            [5, 5, 'deuce'],
+            [4, 3, 'Advantage: Player One'],
+            [3, 4, 'Advantage: Player Two'],
+            [4, 0, 'Winner: Player One'],
+            [0, 4, 'Winner: Player Two'],
+        ];
     }
 }
